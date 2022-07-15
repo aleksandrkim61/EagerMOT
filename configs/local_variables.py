bb9185e1c5504a9b0d8e7403a7da32c41b398404
PiusHwang <bk22106@gmail.com>
2022-07-15 09:38:05 +0900

    mod

--- configs/local_variables.py
+++ configs/local_variables.py
@@ -1,4 +1,4 @@
-MOUNT_PATH = "/content/drive/Mydrive"  # in case you are mounting data storage externally
+MOUNT_PATH = "/content/drive/MyDrive"  # in case you are mounting data storage externally
 SPLIT = 'training'
 
 KITTI_WORK_DIR = MOUNT_PATH + "/storage/eagermot_workspace/kitti"
